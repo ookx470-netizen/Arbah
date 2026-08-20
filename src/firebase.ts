034921f42cecc7d735a2f3a6e8e42c9f92553dbe
ookx470-netizen <ookx470@gmail.com>
2026-08-20 04:11:28 +0300

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,7 +7,7 @@ import firebaseConfig from '../firebase-applet-config.json';
 const app = initializeApp(firebaseConfig);
 
 // Connect directly to the production Firestore database
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId || "ai-studio-imagegallery-a44d0f4b-f3bd-412d-9cdd-d5863b832b78");
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId || "arbah-main");
 export const oldDb = db;
 
 export const storage = getStorage(app);
